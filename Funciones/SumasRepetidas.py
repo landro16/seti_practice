8a12a095d60692e60964d7adf24c8b1b016efd70
landro16.ls@gmail.com <31051212+landro16@users.noreply.github.com>
2023-10-28 15:03:48 -0500

    Reto, multiplicar con sumas sucesivas

---
+++ Funciones/SumasRepetidas.py
@@ -0,0 +1,27 @@
+'''Crear un programa en python que mediante el uso de funciones multiplique dos numeros, solamente usando sumas'''
+
+num1 = int(input("Indique el numero a multiplicar: "))
+num2 = int(input("Indique el numero de veces a multiplicar: "))
+
+
+def multisuma(num1,num2):
+    producto = 0
+    for i in range (1,num2 + 1):
+        producto = producto + num1
+    print(("Producto de las sumas :"),(producto))
+# print (("Producto"),(producto))
+
+multisuma(num1,num2)
+
+
+'''
+producto = 0
+m = int (input ('Ingresa el valor de m: '))
+n = int (input ('Ingresa el valor de n: '))
+for i in range (1, n + 1):
+    producto=producto+m
+    print ()
+print ('Valor de producto: ' + repr (producto))'''
+
+
+
