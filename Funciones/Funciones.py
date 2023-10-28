02241b7f7227fde0fda14f8c551ab4d82b115833
landro16.ls@gmail.com <31051212+landro16@users.noreply.github.com>
2023-10-28 13:57:14 -0500

    Reto duplicar numero con funcion

---
+++ Funciones/Funciones.py
@@ -0,0 +1,9 @@
+'''Escriba un programa en python que ingrese un numero y lo duplice'''
+
+num = int(input("Ingresar numero a duplicar: "))
+
+def duplicar (num):
+    total = num * 2    
+    print (("El doble del numero es: "), (total))
+
+duplicar(num)
